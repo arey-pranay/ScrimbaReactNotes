700158fef7dc35e4daa1d3a2a1ced29268c0947c
arey-pranay <86497126+arey-pranay@users.noreply.github.com>
2023-05-10 20:49:13 +0530

    Update Sec2.js

--- Sec2.js
+++ Sec2.js
@@ -25,3 +25,46 @@ import App from "./App"
 ReactDOM.render(<App />, document.getElementById("root"))
 
 
+--Propsssssssss
+
+----Contact.js
+
+import React from "react"
+export default function Contact(props) {
+    return (
+        <div className="contact-card">
+            <img src={props.img}/>
+            <h3>{props.name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{props.phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{props.email}</p>
+            </div>
+        </div>
+    )
+}
+
+----App.js
+
+function App() {
+    return (
+        <div className="contacts">
+            <Contact 
+                img="./images/mr-whiskerson.png" 
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact 
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+      )
+}
+
+
