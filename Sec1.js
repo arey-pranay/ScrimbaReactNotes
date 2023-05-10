94961f9ea0fd782bf9aeafa269a8a6b019076f8c
arey-pranay <86497126+arey-pranay@users.noreply.github.com>
2023-05-10 20:19:00 +0530

    Update Sec1.js

--- Sec1.js
+++ Sec1.js
@@ -55,9 +55,13 @@ export default function Footer() {
     )
 }
 
-//App.js
+//index.js
 
+import React from "react"
+import ReactDOM from "react-dom"
 import Footer from "./Footer"
+
+ReactDOM.render(<Footer/>, document.getElementById("root"))
 
 --to setup locally
 
