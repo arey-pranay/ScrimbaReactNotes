e9c52b925fa2da69cdfe5437ae130996f06da0a0
arey-pranay <86497126+arey-pranay@users.noreply.github.com>
2023-05-10 20:35:28 +0530

    Update Sec2.js

--- Sec2.js
+++ Sec2.js
@@ -1,5 +1,18 @@
 My doubts: 
-Section vs Nav vs Div 
+Section vs Nav vs Div vs Container
 What's special about the header and footer tags
 Why do we keep parent relative and child absolute
 How do different units of CSS work ??
+Why to put hero--header etc. (double dash in classNames)?
+  
+  
+--Using Javascript inside JSX syntax by the helo if {} curly braces
+function App() {
+    const firstName = "Joe"
+    const lastName = "Schmoe"
+    return (
+        <h1>Hello {firstName} {lastName}!</h1>
+    )
+}
+
+
