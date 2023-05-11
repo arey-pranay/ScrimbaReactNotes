2772c13472af664cc2db7c5ba59c5d99c6023965
arey-pranay <86497126+arey-pranay@users.noreply.github.com>
2023-05-11 17:57:32 +0530

    Update Sec3.js

--- Sec3.js
+++ Sec3.js
@@ -1,5 +1,6 @@
 Doubts:
 Different font size units expkanation. Why is rem better and how exactly does it work ?
+Difference between girds and flexboxes, and how do they work in depth ?
 
 use
 display: flex 
@@ -8,3 +9,5 @@ for navbars or header
 use 
 margin-right:auto 
 to stick stuff to the right
+
+when you use HTML properties in JSX, they're camelCased instead of lowercase. e.g., onclick becomes onCLick.
