67a58559f5fc50e3a271f6b701233c1995154bf3
arey-pranay <86497126+arey-pranay@users.noreply.github.com>
2023-05-11 18:45:43 +0530

    Update Sec3.js

--- Sec3.js
+++ Sec3.js
@@ -1,6 +1,7 @@
 Doubts:
-Different font size units expkanation. Why is rem better and how exactly does it work ?
+Different font size units explanation. Why is rem better and how exactly does it work ?
 Difference between girds and flexboxes, and how do they work in depth ?
+What're hooks ?
 
 use
 display: flex 
@@ -40,3 +41,40 @@ When you render an array on the screen, and then put a button to update the arra
 This is because React doesn't normally care about the local variable changes in your code.
 This is why we need states. To kinda update stuff even more dynamically.
 
+--useState()
+the React.useState() function returns an array of [ "a value", a function()]
+
+--array destructuring
+
+useState returns ["Yes", f()]
+
+ const [whateverWeWant, func] = React.useState("Yes") 
+    console.log(whateverWeWant)
+
+ const result = React.useState("Yes")
+    console.log(result[0])
+
+Both these codes return "Yes", it's just that the upper code destructurizes the array into separate variables upon receiving it.
+
+the func that we get in the React.useState output array, is used to change the value from the array
+
+e.g.,
+    const [count, setCount] = React.useState(0)
+    function add(){
+        setCount(count+1)
+    }
+    return (
+        <div className="counter">
+            <button className="counter--minus">–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button onClick = {add} className="counter--plus">+</button>
+        </div>
+    )
+    
+    DO NOT USE THIS PLEASE EVER   (count = )
+    function add() {
+        setCount(count = count + 1)
+    }
+    
