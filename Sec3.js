7a76a0d321f741166bbd60684ec3226a03fe4038
arey-pranay <86497126+arey-pranay@users.noreply.github.com>
2023-05-11 18:00:32 +0530

    Update Sec3.js

--- Sec3.js
+++ Sec3.js
@@ -11,3 +11,16 @@ margin-right:auto
 to stick stuff to the right
 
 when you use HTML properties in JSX, they're camelCased instead of lowercase. e.g., onclick becomes onCLick.
+if you put a {function()} in your code, it'll get executed as soon as that line is read, so instead, pass that function as a value or an object. e/g/, {function}.
+  
+export default function App() {
+    function handleClick() {
+        console.log("I was clicked!")
+    }
+    return (
+        <div className="container">
+            <img src="https://picsum.photos/640/360" />
+            <button onClick={handleClick}>Click me</button>
+        </div>
+    )
+}
